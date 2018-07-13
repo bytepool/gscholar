de30921052851ddad64ef1052ab20d4b16e24b25
Aljoscha Lautenbach <aljoscha.lautenbach@gmail.com>
2018-07-13 20:52:02 +0200

    Add more options to the ieee_query function.
    It is now possible to
     - choose which field(s) to target for the search string.
     - how many results should be returned (max).
     - add a filter based on start year (i.e., ignore older publications).
     - browse through the results by indicating at which index to start returning results (thus circumventing the max_records of 200).

--- ieeelib/ieeelib.py
+++ ieeelib/ieeelib.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 """
 Library to query IEEE Xplore.
 
@@ -7,19 +6,8 @@ Call the method query with a string which contains the full search
 string. Query will return a list of citations.
 """
 
-try:
-    # python 2
-    from urllib2 import Request, urlopen, quote
-except ImportError:
-    # python 3
-    from urllib.request import Request, urlopen, quote
-
-try:
-    # python 2
-    from htmlentitydefs import name2codepoint
-except ImportError:
-    # python 3
-    from html.entities import name2codepoint
+from urllib.request import Request, urlopen, quote
+from html.entities import name2codepoint
 
 import re
 import os
@@ -29,44 +17,110 @@ import logging
 IEEE_API_VERSION = 1
 IEEE_URL = "http://ieeexploreapi.ieee.org/api/v%s/search/articles" % IEEE_API_VERSION
 
-HEADERS = {'User-Agent': 'Mozilla/5.0'}
+SEARCH_FIELD_NONE = 0
+SEARCH_FIELD_ABSTRACT = 1
+SEARCH_FIELD_DOC_TITLE = 2
+SEARCH_FIELD_PUB_TITLE = 4
+SEARCH_FIELD_AUTHORS = 8
+SEARCH_FIELD_AFFILIATIONS = 16
+SEARCH_FIELD_KEYWORDS = 32
 
-FORMAT_BIBTEX = 4
-FORMAT_ENDNOTE = 3
-FORMAT_REFMAN = 2
-FORMAT_WENXIANWANG = 5
+SEARCH_FIELDS = {SEARCH_FIELD_ABSTRACT:'"Abstract"', SEARCH_FIELD_DOC_TITLE:'"Document Title"', SEARCH_FIELD_AUTHORS:'"Authors"', SEARCH_FIELD_PUB_TITLE:'"Publication Title"', SEARCH_FIELD_AFFILIATIONS:'"Author Affiliations"', SEARCH_FIELD_KEYWORDS:'"Author Keywords"'}
+
+AND = " AND "
+OR = " OR "
+
+HEADERS = {'User-Agent': 'Mozilla/5.0'}
 
 logger = logging.getLogger(__name__)
 
-def query(searchstr, apikey="", outformat=FORMAT_BIBTEX, allresults=False):
+def determine_query_fields(fields_mask):
+    """
+    Determine which fields should be used in the query. 
+    """
+    fields = []
+
+    if fields_mask & SEARCH_FIELD_ABSTRACT:
+        fields.append(SEARCH_FIELDS[SEARCH_FIELD_ABSTRACT])
+    if fields_mask & SEARCH_FIELD_PUB_TITLE:
+        fields.append(SEARCH_FIELDS[SEARCH_FIELD_PUB_TITLE])
+    if fields_mask & SEARCH_FIELD_DOC_TITLE:
+        fields.append(SEARCH_FIELDS[SEARCH_FIELD_DOC_TITLE])
+    if fields_mask & SEARCH_FIELD_AUTHORS:
+        fields.append(SEARCH_FIELDS[SEARCH_FIELD_AUTHORS])
+    if fields_mask & SEARCH_FIELD_AFFILIATIONS:
+        fields.append(SEARCH_FIELDS[SEARCH_FIELD_AFFILIATIONS])
+    if fields_mask & SEARCH_FIELD_KEYWORDS:
+        fields.append(SEARCH_FIELDS[SEARCH_FIELD_KEYWORDS])
+
+    return fields
+
+def populate_query_fields(fields, search_str, operator):
+    """
+    Use the given list of fields 
+    """
+    base_param = "?querytext="
+    if not fields:
+        query = base_param + quote(search_str)
+        return query
+
+    query = base_param
+    for field in fields:
+        query = query + "(%s:%s)%s" % (quote(field), quote(search_str), quote(operator))
+
+    len_op = len(quote(operator))
+    return query[:-len_op]
+
+
+def query(search_str, api_key="", start_record=1, max_records=200, start_year=None, fields_mask=SEARCH_FIELD_ABSTRACT, operator=AND):
     """Query IEEE Xplore.
 
-    This method queries IEEE Xplore and returns a list of citations.
+    This method queries IEEE Xplore and returns a json string with the result. 
 
     Parameters
     ----------
-    searchstr : str
+    search_str : str
         the query
-    apikey : str
+    api_key : str
         the key to be able to use IEEE's API
-    outformat : int, optional
-        the output format of the citations. Default is bibtex.
-    allresults : bool, optional
-        return all results or only the first (i.e. best one)
+    start_record : int, optional
+        the index of the first record which should be retrieved (used to "browse" through results)
+    max_records : int, optional
+        the number of records to return (default is 200, the maximum currently allowed by IEEE).
 
     Returns
     -------
-    result : list of strings
-        the list with citations
-
+    result : json string
+        the json string with the result of the query
     """
-    logger.debug("Query: {sstring}".format(sstring=searchstr))
-    searchstr = '?querytext=' + quote(searchstr)
-    apistr = "&apikey=" + apikey
-    url = IEEE_URL + searchstr + apistr
+    logger.debug("Query: {sstring}".format(sstring=search_str))
+
+    fields = determine_query_fields(fields_mask)
+    # query_str must be first param in URL
+    query_str = populate_query_fields(fields, search_str, operator)
+    
+    #query_str = '?querytext=%s' % quote(search_str)
+    #query_str = '?abstract=%s' % quote(search_str)
+    #query_str = '?article_title=%s' % quote(search_str)
+    #query_str = '?article_title=%s&abstract=%s' % (quote(search_str),quote(search_str))
+    #field = '"Abstract"'
+    #query_str = '?querytext=("%s":%s)' % (field, quote(search_str))
+    
+    start_record_str = "&start_record=%d" % start_record
+    max_records_str = "&max_records=%d" % max_records
+    if start_year:
+        start_year_str = "&start_year=%d" % start_year
+    else:
+        start_year_str = ""
+
+    api_str = "&apikey=%s" % quote(api_key) # last param in URL
+    
+    params = start_year_str + max_records_str + start_record_str
+    url = IEEE_URL + query_str + params  + api_str
+    #print("DEBUG: Query URL:", url)
     
     header = HEADERS
-    #header['Cookie'] = "GSP=CF=%d" % outformat
+    #header['Cookie'] = "GSP=CF=%d" % outformat for google scholar
     request = Request(url, headers=header)
     response = urlopen(request)
 
